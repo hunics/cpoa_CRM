6405b772773668114d004fb18a47deaf2918ea8b
hunics <jp.software.peticom@gmail.com>
2014-02-28 12:17:32 -0600

    Update getUsersFromLoginTBL2json.php

--- Logic_Model/getUsersFromLoginTBL2json.php
+++ Logic_Model/getUsersFromLoginTBL2json.php
@@ -1,33 +1,64 @@
 <?php
-@session_start();
 require_once("../FrameWork/Mysqli_model.php");
+
 
 class getUsers {
     
     private $dBManager;
     
     public function __construct() {
+		$this->getSalesRepS = array();
+		$this->getRefferalRepS = array();
         $this->dBManager = new Mysqli_model();
     }
     
     
     private function getReferralRep($ReferralRepID){
+		/*
+		 * speeder ini 2 Read from Array
+		 */ 
+			if(array_key_exists($ReferralRepID,$this->getRefferalRepS)){
+				$rr =  $this->getRefferalRepS[$ReferralRepID];
+				return $rr[0]. "  ". $rr[1];
+			}
+		/*
+		 * speeder end
+		 */ 
+		
         $R_queryje = "SELECT * FROM `referralreps` WHERE ReferralRepID =".$ReferralRepID;
         $R_result = $this->dBManager->getRow($R_queryje);
         if($R_result){
+			// - speeder add to Array ini
+			$this->getRefferalRepS[$ReferralRepID] = array($R_result["FirstName"],$R_result["LastName"]);
+            // - speeder add end
             return $R_result["FirstName"]. " ".$R_result["LastName"];
         }else{
+			$this->getRefferalRepS[$ReferralRepID] = array("--","--");
             return "---";
         }
     }
  
     
     private function getSalesRep($SalesRepID){
+		/*
+		 * speeder ini 2 Read from Array
+		 */ 
+			if(array_key_exists(SalesRepID,$this->getSalesRepS)){
+				$ri =  $this->getSalesRepS[$SalesRepID];
+				return $ri[0]. "  ". $ri[1];
+			}
+		/*
+		 * speeder end
+		 */ 
         $R_queryje = "SELECT * FROM `salesreps` WHERE SalesRepID =".$SalesRepID;
         $R_result = $this->dBManager->getRow($R_queryje);
         if($R_result){
+			// - speeder add to Array ini
+			$this->getSalesRepS[$SalesRepID] = array($R_result["FirstName"],$R_result["LastName"]);
+            // - speeder add end
             return $R_result["FirstName"]. " ".$R_result["LastName"];
         }else{
+			$this->getSalesRepS[$SalesRepID] = array("--","--");
             return "---";
         }
     }    
