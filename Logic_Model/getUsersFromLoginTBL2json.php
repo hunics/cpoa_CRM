9fcd57f102d10acd27dfa118fd9f30b52a13257f
hunics <jp.software.peticom@gmail.com>
2014-02-28 12:24:26 -0600

    Update getUsersFromLoginTBL2json.php

--- Logic_Model/getUsersFromLoginTBL2json.php
+++ Logic_Model/getUsersFromLoginTBL2json.php
@@ -14,52 +14,52 @@ class getUsers {
     
     
     private function getReferralRep($ReferralRepID){
-		/*
-		 * speeder ini 2 Read from Array
-		 */ 
-			if(array_key_exists($ReferralRepID,$this->getRefferalRepS)){
-				$rr =  $this->getRefferalRepS[$ReferralRepID];
-				return $rr[0]. "  ". $rr[1];
-			}
-		/*
-		 * speeder end
-		 */ 
+	/*
+	* speeder ini 2 Read from Array
+	*/ 
+	if(array_key_exists($ReferralRepID,$this->getRefferalRepS)){
+		$rr =  $this->getRefferalRepS[$ReferralRepID];
+			return $rr[0]. "  ". $rr[1];
+	}
+	/*
+	* speeder end
+	*/ 
 		
         $R_queryje = "SELECT * FROM `referralreps` WHERE ReferralRepID =".$ReferralRepID;
         $R_result = $this->dBManager->getRow($R_queryje);
         if($R_result){
-			// - speeder add to Array ini
-			$this->getRefferalRepS[$ReferralRepID] = array($R_result["FirstName"],$R_result["LastName"]);
-            // - speeder add end
-            return $R_result["FirstName"]. " ".$R_result["LastName"];
+		// - speeder add to Array ini
+		$this->getRefferalRepS[$ReferralRepID] = array($R_result["FirstName"],$R_result["LastName"]);
+            	// - speeder add end
+            	return $R_result["FirstName"]. " ".$R_result["LastName"];
         }else{
-			$this->getRefferalRepS[$ReferralRepID] = array("--","--");
-            return "---";
+		$this->getRefferalRepS[$ReferralRepID] = array("--","--");
+            	return "---";
         }
     }
  
     
     private function getSalesRep($SalesRepID){
-		/*
-		 * speeder ini 2 Read from Array
-		 */ 
-			if(array_key_exists(SalesRepID,$this->getSalesRepS)){
-				$ri =  $this->getSalesRepS[$SalesRepID];
-				return $ri[0]. "  ". $ri[1];
-			}
-		/*
-		 * speeder end
-		 */ 
+	/*
+	* speeder ini 2 Read from Array
+	*/ 
+	if(array_key_exists(SalesRepID,$this->getSalesRepS)){
+		$ri =  $this->getSalesRepS[$SalesRepID];
+		return $ri[0]. "  ". $ri[1];
+	}
+	/*
+	* speeder end
+	*/ 
         $R_queryje = "SELECT * FROM `salesreps` WHERE SalesRepID =".$SalesRepID;
         $R_result = $this->dBManager->getRow($R_queryje);
         if($R_result){
-			// - speeder add to Array ini
-			$this->getSalesRepS[$SalesRepID] = array($R_result["FirstName"],$R_result["LastName"]);
-            // - speeder add end
+		// - speeder add to Array ini
+		$this->getSalesRepS[$SalesRepID] = array($R_result["FirstName"],$R_result["LastName"]);
+            	// - speeder add end
             return $R_result["FirstName"]. " ".$R_result["LastName"];
         }else{
-			$this->getSalesRepS[$SalesRepID] = array("--","--");
-            return "---";
+		$this->getSalesRepS[$SalesRepID] = array("--","--");
+            	return "---";
         }
     }    
     
